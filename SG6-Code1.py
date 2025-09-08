a525e9a337d689c2fcfe43127c36182dc138f136
LilaMadisonCFernandez <lilamadison.fernandez@smc.pshs.edu.ph>
2025-09-08 10:16:28 +0800

    Add files via upload
    Code 1 lets the user input three numbers and it adds them. The output will also showcase two decimal places.

---
+++ SG6-Code1.py
@@ -0,0 +1,8 @@
+float_num1 = float(input("Enter a number"))
+float_num2 = float(input("Enter a number"))
+float_num3 = float(input("Enter a number"))
+
+sum = float_num1 + float_num2 + float_num3
+sum = "{:.2f}".format(sum)
+
+print(sum)
